56a34f85bc15c7beec4695bbdc716e44debf438c
aledgriffiths79 <aledgriffiths08@hotmail.co.uk>
2020-03-16 13:07:28 +0000

    Haley from code institute says 'Travis is a bit picky about the yml file. I'd recommend copying/ pasting the code exactly  as it appears in the mini project and then trying to run it that way. '

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+
+# And we're going to just register our Post class with our admin site.
+admin.site.register(Post)
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Post(models.Model):
+  """
+  A single Blog post
+  """
+  title = models.Charfield(max_length=200)
+  content = models.TextField()
+  created_date = models.DateTimeField(auto_now_add=True)
+  published_date = models.DateTimeField(blank=True, null=True, default=timezone.now)
+  views = models.IntegerField(default=0)
+  tag = models.Charfield(max_length=30, blank=True, null=True)
+  image = models.ImageField(upload_to='img', blank=True, null=True)
+
+  def _unicode_(self):
+    return self.title
+
+
