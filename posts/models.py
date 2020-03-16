1839391037508a06ee230183de4a1714769735f0
aledgriffiths79 <aledgriffiths08@hotmail.co.uk>
2020-03-16 13:39:49 +0000

    wrong syntax. CharField instead of Charfield

--- posts/models.py
+++ posts/models.py
@@ -7,12 +7,12 @@ class Post(models.Model):
   """
   A single Blog post
   """
-  title = models.Charfield(max_length=200)
+  title = models.CharField(max_length=200)
   content = models.TextField()
   created_date = models.DateTimeField(auto_now_add=True)
   published_date = models.DateTimeField(blank=True, null=True, default=timezone.now)
   views = models.IntegerField(default=0)
-  tag = models.Charfield(max_length=30, blank=True, null=True)
+  tag = models.CharField(max_length=30, blank=True, null=True)
   image = models.ImageField(upload_to='img', blank=True, null=True)
 
   def _unicode_(self):
