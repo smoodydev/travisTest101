454912364e43b6a903d11b70fc35da27354db145
aledgriffiths79 <aledgriffiths08@hotmail.co.uk>
2020-03-23 18:01:48 +0000

    allowed_hosts

--- blog/settings.py
+++ blog/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ 'blog-test-app-code-institute.herokuapp.com' ]
+ALLOWED_HOSTS = [ '127.0.0.1', 'blog-test-app-code-institute.herokuapp.com' ]
 
 
 # Application definition
